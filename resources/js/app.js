3e695437c5c170d474e2c122259ed2ebd2460d89
vagrant <vagrant@homestead>
2019-01-20 08:32:39 +0000

    add npm install -D vue-router

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,9 +5,9 @@
 //  * building robust, powerful web applications using Vue and Laravel.
 //  */
 
-// require('./bootstrap');
+require('./bootstrap');
 
-// window.Vue = require('vue');
+window.Vue = require('vue');
 
 // /**
 //  * The following block of code may be used to automatically register your
@@ -20,7 +20,7 @@
 // // const files = require.context('./', true, /\.vue$/i)
 // // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 // /**
 //  * Next, we will create a fresh Vue application instance and attach it to
@@ -32,9 +32,22 @@
 //     el: '#app'
 // });
 
+// import Vue from 'vue'
+
+// new Vue({
+//   el: '#app',
+//   template: '<h1>Hello world</h1>'
+// })
+
 import Vue from 'vue'
+// ルーティングの定義をインポートする
+import router from './router'
+// ルートコンポーネントをインポートする
+import App from './App.vue'
 
 new Vue({
   el: '#app',
-  template: '<h1>Hello world</h1>'
+  router, // ルーティングの定義を読み込む
+  components: { App }, // ルートコンポーネントの使用を宣言する
+  template: '<App />' // ルートコンポーネントを描画する
 })
